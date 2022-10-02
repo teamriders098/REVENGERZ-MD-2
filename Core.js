1f53519af66576ca1fbf9353280a617b258d0e08
3.69_pika <106071901+Pika4O4@users.noreply.github.com>
2022-10-02 14:46:38 +0530

    Create Core.js

--- Core.js
+++ Core.js
@@ -4191,7 +4191,7 @@ Miku.sendMessage(from, { react: { text: `${global.reactmoji}`, key: m.key }})
             { quickReplyButton: { displayText: `List Menu 🍢`, id: 'command'} },
             { quickReplyButton: { displayText: `Owner 🤣`, id: 'owner'} }
         	]
-        	Miku.sendMessage(m.chat, { caption: menulist, video:fs.readFileSync('./system/miku2.mp4'),gifPlayback:true, mimetype: `${docs}`, fileName: `${ownername}`, templateButtons: buttonmenu, footer: `${botname}`, mentionedJid: [m.sender] })
+        	Miku.sendMessage(m.chat, { caption: menulist, video:fs.readFileSync('./system/miku2.mp4'),gifPlayback:true, mimetype: `video/mp4`, fileName: `${ownername}`, templateButtons: buttonmenu, footer: `${botname}`, mentionedJid: [m.sender] })
                         }
                      }
             break
